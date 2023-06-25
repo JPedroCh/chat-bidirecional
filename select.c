90c28eb7e360b561cb8aca118222908bb9399f25
João Pedro Chaves <joaopedroaschaves@gmail.com>
2023-06-25 18:56:05 -0300

    feat: adicao das funcionalidades gerenciais de criacao e exclusao de salas.
    Co-authored-by: nYCSTs <lucascandradex@protonmail.com>

--- select.c
+++ select.c
@@ -6,11 +6,16 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pthread.h>
 
-#define MAX_CLIENTS 10
+// #define MAX_CLIENTS 10
 #define MAX_ROOMS 5
 #define MAX_ROOM_NAME_LENGTH 50
 #define MAX_MESSAGE_SIZE 500
+#define MAX_COMMAND_LENGTH 50
+
+int currentMaxRooms = 0;
+int closedServer = 0;
 
 struct Client {
   int fd;
@@ -19,18 +24,27 @@ struct Client {
 
 struct Room {
   int roomNumber;
-  struct Client clients[MAX_CLIENTS];
+  struct Client* clients;
+  int maxClients;
   int numClients;
   char name[MAX_ROOM_NAME_LENGTH + 1];
 };
 
+struct ServerData {
+    struct Room rooms[MAX_ROOMS];
+    pthread_t commandThread;
+    int listener;
+    int fdmax;
+    fd_set master;
+};
+
 char *listRooms(struct Room rooms[]) {
   char tmp[MAX_ROOM_NAME_LENGTH * 25];
-  char *roomMessage = malloc(MAX_ROOMS * MAX_ROOM_NAME_LENGTH * 25 * sizeof(char));
+  char *roomMessage = malloc(currentMaxRooms * MAX_ROOM_NAME_LENGTH * 25 * sizeof(char));
 
   sprintf(roomMessage, "\nSalas:\n");
-  for (int i = 0; i < MAX_ROOMS; i++) {
-    sprintf(tmp, "  Room %d: %s (%d/%d clients)\n", rooms[i].roomNumber, rooms[i].name, rooms[i].numClients, MAX_CLIENTS);
+  for (int i = 0; i < currentMaxRooms; i++) {
+    sprintf(tmp, "  Room %d: %s (%d/%d clients)\n", rooms[i].roomNumber, rooms[i].name, rooms[i].numClients, rooms[i].maxClients);
     strcat(roomMessage, tmp);
   }
   strcat(roomMessage, "\n");
@@ -38,12 +52,27 @@ char *listRooms(struct Room rooms[]) {
   return roomMessage;
 };
 
+void listRoomsServer(struct Room rooms[]) {
+  char tmp[MAX_ROOM_NAME_LENGTH * 25];
+  char *roomMessage = malloc(currentMaxRooms * MAX_ROOM_NAME_LENGTH * 25 * sizeof(char));
+
+  sprintf(roomMessage, "\nSalas:\n");
+  for (int i = 0; i < currentMaxRooms; i++) {
+    sprintf(tmp, "[INFO]  Room ID %d: %s (%d/%d clients)\n", rooms[i].roomNumber, rooms[i].name, rooms[i].numClients, rooms[i].maxClients);
+    strcat(roomMessage, tmp);
+  }
+  strcat(roomMessage, "\n");
+
+  printf("%s", roomMessage);
+
+};
+
 void handleNewClient(struct Room rooms[], int newfd, int *fdmax) {
   int roomIndex = -1;
   char message[MAX_MESSAGE_SIZE];
 
 
-  if (rooms[0].numClients < MAX_CLIENTS) {
+  if (rooms[0].numClients < rooms[0].maxClients) {
     roomIndex = 0;
   }
 
@@ -60,7 +89,7 @@ void handleNewClient(struct Room rooms[], int newfd, int *fdmax) {
     *fdmax = newfd;
   }
 
-  snprintf(message, sizeof(message), "\033[2J\033[HBem vindo a sala principal.\nPrimeiro defina seu nome utilizando o comando \\name <NAME>. Depois disso verifique as salas disponiveis utilizando \\list e entao \\join <ROOM_ID> para entrar em uma sala.\n\n");
+  snprintf(message, sizeof(message), "\033[2J\033[HBem vindo a sala principal.\nPrimeiro defina seu nome utilizando o comando /name <NAME>. Depois disso verifique as salas disponiveis utilizando \\list e entao \\join <ROOM_ID> para entrar em uma sala.\n\n");
   send(newfd, message, strlen(message), 0);
 
   printf("Novo cliente conectou-se a sala principal\n");
@@ -87,8 +116,8 @@ void handleReceivedData(struct Room rooms[], int i, char *buf) {
   int indexClient = -1;
   char message[MAX_MESSAGE_SIZE];
 
-  for (int j = 0; j < MAX_ROOMS && indexRoom < 0; j++) {
-    for (int k = 0; k < MAX_CLIENTS; k++) {
+  for (int j = 0; j < currentMaxRooms && indexRoom < 0; j++) {
+    for (int k = 0; k < rooms[j].numClients; k++) {
       if (rooms[j].clients[k].fd == i) {
         indexRoom = j;
         indexClient = k;
@@ -120,7 +149,7 @@ void handleReceivedData(struct Room rooms[], int i, char *buf) {
       } else if (strncmp(buf, "/join ", 6) == 0) {
         printf("Cliente %d na sala %d executou o comando /join\n", i, rooms[indexRoom].roomNumber);
         int requestedRoom = atoi(buf + 6);
-        if (requestedRoom > 0 && requestedRoom <= MAX_ROOMS) {
+        if (requestedRoom > 0 && requestedRoom <= currentMaxRooms && rooms[requestedRoom - 1].numClients < rooms[requestedRoom - 1].maxClients) {
           rooms[indexRoom].clients[indexClient].fd = -1;
           rooms[indexRoom].numClients--;
 
@@ -172,79 +201,186 @@ void handleReceivedData(struct Room rooms[], int i, char *buf) {
   }
 }
 
+void removeClientFromRoom(struct Room rooms[], int fd){
+  for (int i = 0; i < currentMaxRooms; i++) {
+    for (int j = 0; j < rooms[i].numClients; j++) {
+      if (rooms[i].clients[j].fd == fd) {
+        rooms[i].clients[j].fd = -1;
+        rooms[i].numClients--;
+        return;
+      }
+    }
+  }
+}
+
+void createRoom(struct Room rooms[]) {
+  if (currentMaxRooms == MAX_ROOMS) {
+    printf("Numero maximo de salas atingido.\n");
+    return;
+  }
+
+  char name[MAX_ROOM_NAME_LENGTH];
+  int maxRoomClients;
+  printf("Digite o nome da sala: ");
+  scanf("%s", name);
+  printf("Digite o número máximo de usuários da sala\n");
+  scanf("%d", &maxRoomClients);
+
+  int roomNumber = currentMaxRooms + 1;
+  rooms[currentMaxRooms].roomNumber = roomNumber;
+  rooms[currentMaxRooms].numClients = 0;
+  rooms[currentMaxRooms].maxClients = maxRoomClients;
+  rooms[currentMaxRooms].clients = malloc(sizeof(struct Client) * maxRoomClients);
+  snprintf(rooms[currentMaxRooms].name, MAX_ROOM_NAME_LENGTH + 1,  "%s", name);
+
+  currentMaxRooms++;
+
+  printf("Sala com ID \"%d\" criada com sucesso.\n", roomNumber);
+}
+
+void deleteRoom(struct Room rooms[]) {
+  int roomNumber;
+  printf("Digite o numero da sala: ");
+  scanf("%d", &roomNumber);
+
+  if (roomNumber > currentMaxRooms || roomNumber <= 0) {
+    printf("Sala invalida.\n");
+    return;
+  }
+
+  if (roomNumber == 1) {
+    printf("Não é possivel deletar a sala principal.\n");
+    return;
+  }
+
+  for (int i = rooms[roomNumber - 1].numClients - 1; i >= 0; i--) {
+    rooms[0].numClients++;
+    rooms[0].clients[rooms[0].numClients].fd = rooms[roomNumber - 1].clients[i].fd;
+    strcpy(rooms[0].clients[rooms[0].numClients].name, rooms[roomNumber - 1].clients[i].name);
+    rooms[roomNumber - 1].clients[i].fd = -1;
+    snprintf(rooms[roomNumber - 1].name, MAX_ROOM_NAME_LENGTH + 1,  "");
+  }
+  rooms[roomNumber - 1].numClients = 0;
+
+  for (int i = roomNumber - 1; i < currentMaxRooms - 1; i++) {
+    rooms[i] = rooms[i + 1];
+  }
+
+  currentMaxRooms--;
+
+  printf("Sala com ID \"%d\" deletada com sucesso.\n", roomNumber);
+}
+
+void* commandInput(void* arg) {
+    struct ServerData *serverData = (struct ServerData*)arg;
+    char command[MAX_COMMAND_LENGTH];
+    
+    while (1) {
+        printf("\nEnter command: ");
+        scanf("%s", command);
+        
+        if (strcmp(command, "/list") == 0) {
+            listRoomsServer(serverData->rooms);
+        } else if (strcmp(command, "/create") == 0) {
+            createRoom(serverData->rooms);
+        } else if (strcmp(command, "/delete") == 0) {
+            deleteRoom(serverData->rooms);
+        } else if (strcmp(command, "/exit") == 0) {
+            exit(0);
+            break;
+        } else {
+            printf("Invalid command.\n");
+        }
+    }
+    
+    pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]) {
-  int listener, newfd, fdmax, i, nbytes;
+  int newfd, i, nbytes;
   struct sockaddr_in myaddr, remoteaddr;
   socklen_t addrlen;
   char buf[256];
-  fd_set master, read_fds;
+  fd_set read_fds;
   int yes = 1;
+  struct ServerData serverData;
 
   if (argc < 3) {
     printf("Digite IP e Porta para este servidor\n");
     exit(1);
-  }
+  } 
 
   // Initialize room data
-  struct Room rooms[MAX_ROOMS];
-  for (int i = 0; i < MAX_ROOMS; i++) {
-    rooms[i].roomNumber = i + 1;
-    rooms[i].numClients = 0;
-    snprintf(rooms[i].name, MAX_ROOM_NAME_LENGTH + 1, "Room %d", i + 1);
-  }
+  serverData.rooms[0].roomNumber = 1;
+  serverData.rooms[0].numClients = 0;
+  snprintf(serverData.rooms[0].name, MAX_ROOM_NAME_LENGTH + 1, "Lounge");
+  serverData.rooms[0].maxClients = 50;
+  serverData.rooms[0].clients = malloc(sizeof(struct Client) * 50);
+  currentMaxRooms = 1;
+  
 
-  FD_ZERO(&master);
+  FD_ZERO(&(serverData.master));
   FD_ZERO(&read_fds);
 
-  listener = socket(AF_INET, SOCK_STREAM, 0);
-  if (listener < 0) {
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+
+  serverData.listener = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverData.listener < 0) {
     perror("socket");
     exit(1);
   }
 
-  setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+  setsockopt(serverData.listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
   myaddr.sin_family = AF_INET;
   myaddr.sin_addr.s_addr = inet_addr(argv[1]);
   myaddr.sin_port = htons(atoi(argv[2]));
   memset(&(myaddr.sin_zero), '\0', 8);
 
-  if (bind(listener, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+  if (bind(serverData.listener, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
     perror("bind");
     exit(1);
   }
 
-  if (listen(listener, 10) < 0) {
+  if (listen(serverData.listener, 10) < 0) {
     perror("listen");
     exit(1);
   }
 
-  FD_SET(listener, &master);
-  fdmax = listener;
+  FD_SET(serverData.listener, &(serverData.master));
+  serverData.fdmax = serverData.listener;
 
   printf("Server is running. Waiting for connections...\n");
 
+  // Create command input thread
+    if (pthread_create(&(serverData.commandThread), NULL, commandInput, (void*)&serverData) != 0) {
+        perror("pthread_create");
+        exit(1);
+    }
+
   for (;;) {
-    read_fds = master;
-    if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
-      perror("select");
+    read_fds = (serverData.master);
+    if (select(serverData.fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
+      perror("[Select]");
       exit(1);
     }
 
-    for (i = 0; i <= fdmax; i++) {
+    for (i = 0; i <= serverData.fdmax; i++) {
       if (FD_ISSET(i, &read_fds)) {
-        if (i == listener) {
+        if (i == serverData.listener) {
           addrlen = sizeof(remoteaddr);
-          newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
+          newfd = accept(serverData.listener, (struct sockaddr *)&remoteaddr, &addrlen);
           if (newfd == -1) {
             perror("accept");
           } else {
-            FD_SET(newfd, &master);
-            if (newfd > fdmax) {
-              fdmax = newfd;
+            FD_SET(newfd, &(serverData.master));
+            if (newfd > serverData.fdmax) {
+              serverData.fdmax = newfd;
             }
 
-            handleNewClient(rooms, newfd, &fdmax);
+            handleNewClient(serverData.rooms, newfd, &serverData.fdmax);
           }
         } else {
           memset(buf, 0, sizeof(buf));
@@ -252,13 +388,15 @@ int main(int argc, char *argv[]) {
           if (nbytes <= 0) {
             if (nbytes == 0) {
               printf("Client %d disconnected\n", i);
+              removeClientFromRoom(serverData.rooms, i);
+              
             } else {
               perror("recv");
             }
             close(i);
-            FD_CLR(i, &master);
+            FD_CLR(i, &(serverData.master));
           } else {
-            handleReceivedData(rooms, i, buf);
+            handleReceivedData(serverData.rooms, i, buf);
           }
         }
       }
